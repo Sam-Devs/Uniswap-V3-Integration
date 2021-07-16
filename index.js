16b318df2f36b7a1f2028e95a5cd8aedc377587a
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-16 18:27:05 +0100

    integration

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
 const { ethers } = require("ethers");
 const { Token } = require("@uniswap/sdk-core");
 const { Pool } = require("@uniswap/v3-sdk");
+
+const maninnetProvider = process.env.MAINNET;
+const provider = new ethers.providers.JsonRpcProvider(maninnetProvider);
+
