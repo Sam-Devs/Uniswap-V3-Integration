905f13314118ff92ff9432935de72fca384a2732
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-17 20:10:06 +0100

    liquidity file updated

--- addLiquidity.js
+++ addLiquidity.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const { ethers } = require("ethers");
-const ISwapRouterABI = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json");
+const INonfungiblePositionManager = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json");
 
 // Provider
 const maninnetProvider = process.env.MAINNET;
 const provider = new ethers.providers.JsonRpcProvider(maninnetProvider);
 
-const routerAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
-const routerContract = new ethers.Contract(routerAddress, ISwapRouterABI.abi, provider);
+
+// Signer
+const signer = new ethers.Wallet.createRandom();
+const account = signer.connect(provider);
+
+const positionManagerAddress = "0xC36442b4a4522E871399CD717aBDD847Ab11FE88";
+const positionManagerContract = new ethers.Contract(positionManagerAddress, INonfungiblePositionManager.abi, provider);
+
+const positionManager = positionManagerContract.connect(account);
+
+console.log(positionManager);
