18b6a115949510c65a08554dbe7f4b32afe0678a
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-17 16:07:43 +0100

    swap.js

--- swap.js
+++ swap.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { ethers } = require("ethers");
+const { Token, CurrencyAmount } = require("@uniswap/sdk-core");
 const ISwapRouterABI = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json");
 const { Trade, Route } = require("@uniswap/v3-sdk");
 
@@ -9,9 +10,7 @@ const provider = new ethers.providers.JsonRpcProvider(maninnetProvider);
 
 // Signer
 const signer = new ethers.Wallet.createRandom();
-console.log(signer);
 const account = signer.connect(provider);
-console.log(account);
 
 const routerAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 const routerContract = new ethers.Contract(
@@ -20,36 +19,50 @@ const routerContract = new ethers.Contract(
   provider
 );
 
+
+const tokenAddresses = {
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    // poolAddress: "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+}
+
 const router = routerContract.connect(account);
-console.log(router);
 
 const main = async () => {
-  const deadline = Math.floor(Date.now() / 1000) + 60 + 20;
-  const amountIn = CurrencyAmount.fromRawAmount(
-    tokenAddresses.token0,
-    "5000000000"
-  );
-  const route = new Route([pool], tokenAddresses.token0, tokenAddresses.token1);
-  console.log(
-    `1 USDC can be swapped for ${route.midPrice.toSignificant(6)} WETH`
-  );
-  console.log(
-    `1 WETH can be swapped for ${route.midPrice.invert.toSignificant(9)} USDC`
-  );
-  const trade = new Trade.exactIn(route, amountIn);
-  console.log(
-    `The execution price of this trade is ${trade.executionPrice.toSignificant(
-      6
-    )} WETH for 1 USDC`
-  );
+  try {
+    const deadline = Math.floor(Date.now() / 1000) + 60 + 20;
+    const amountIn = CurrencyAmount.fromRawAmount(
+      tokenAddresses.token0,
+      "5000000000"
+    );
+    const route = new Route(
+      [pool],
+      tokenAddresses.token0,
+      tokenAddresses.token1
+    );
+    console.log(
+      `1 USDC can be swapped for ${route.midPrice.toSignificant(6)} WETH`
+    );
+    console.log(
+      `1 WETH can be swapped for ${route.midPrice.invert.toSignificant(9)} USDC`
+    );
+    const trade = new Trade.exactIn(route, amountIn);
+    console.log(
+      `The execution price of this trade is ${trade.executionPrice.toSignificant(
+        6
+      )} WETH for 1 USDC`
+    );
 
-  // const swapParams = {
-  //     path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
-  //     recipient: signer.address,
-  //     deadline: deadline,
-  //     amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
-  //     amountOutMinimum:
-  // }
-  // const swapTransaction = router.exactInput(swapParams, {value: value, gasPrice: 20e9})
+    // const swapParams = {
+    //     path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
+    //     recipient: signer.address,
+    //     deadline: deadline,
+    //     amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
+    //     amountOutMinimum:
+    // }
+    // const swapTransaction = router.exactInput(swapParams, {value: value, gasPrice: 20e9})
+  } catch (error) {
+    console.log(error);
+  }
 };
 main();
