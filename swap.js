8ee2693c76aece92186b46b2c00b74aecc0214c3
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-17 15:56:41 +0100

    swap.js

--- swap.js
+++ swap.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
+const { ethers } = require("ethers");
 const ISwapRouterABI = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json");
 const { Trade, Route } = require("@uniswap/v3-sdk");
 
-const signer = new ethers.Wallet.createRandom();
-const account = signer.connect(provider);
-
-const routerAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+// Provider
 const maninnetProvider = process.env.MAINNET;
 const provider = new ethers.providers.JsonRpcProvider(maninnetProvider);
+
+// Signer
+const signer = new ethers.Wallet.createRandom();
+console.log(signer);
+const account = signer.connect(provider);
+console.log(account);
+
+const routerAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 const routerContract = new ethers.Contract(
   routerAddress,
   ISwapRouterABI.abi,
@@ -15,8 +21,14 @@ const routerContract = new ethers.Contract(
 );
 
 const router = routerContract.connect(account);
+console.log(router);
 
 const main = async () => {
+  const deadline = Math.floor(Date.now() / 1000) + 60 + 20;
+  const amountIn = CurrencyAmount.fromRawAmount(
+    tokenAddresses.token0,
+    "5000000000"
+  );
   const route = new Route([pool], tokenAddresses.token0, tokenAddresses.token1);
   console.log(
     `1 USDC can be swapped for ${route.midPrice.toSignificant(6)} WETH`
@@ -32,12 +44,12 @@ const main = async () => {
   );
 
   // const swapParams = {
-    //     path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
-    //     recipient: signer.address,
-    //     deadline: deadline,
-    //     amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
-    //     amountOutMinimum: 
-    // }
-    const swapTransaction = router.exactInput(swapParams, {value: value, gasPrice: 20e9})
+  //     path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
+  //     recipient: signer.address,
+  //     deadline: deadline,
+  //     amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
+  //     amountOutMinimum:
+  // }
+  // const swapTransaction = router.exactInput(swapParams, {value: value, gasPrice: 20e9})
 };
 main();
