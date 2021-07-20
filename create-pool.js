1ac628d380defdfec0bc0236a44580449212650e
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-20 06:52:34 +0100

    create-pool file

--- create-pool.js
+++ create-pool.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const { ethers} = require("ethers");
+const { ethers } = require("ethers");
 const IUniswapV3Pool = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
-const { Token} = require("@uniswap/sdk-core");
-const { Pool, TickListDataProvider, Tick} = require("@uniswap/v3-sdk")
+const { Token } = require("@uniswap/sdk-core");
+const { Pool, TickListDataProvider, Tick } = require("@uniswap/v3-sdk");
 
 //  pool contract address
 const poolAddress = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8";
@@ -10,19 +10,30 @@ const mainnet = process.env.MAINNET;
 const provider = new ethers.providers.JsonRpcProvider(mainnet);
 
 // Pool Contract
-const poolContract = new ethers.Contract(poolAddress, IUniswapV3Pool.abi, provider);
+const poolContract = new ethers.Contract(
+  poolAddress,
+  IUniswapV3Pool.abi,
+  provider
+);
 
 // Token Address Object
 const tokenAddresses = {
-    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-    token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
-}
+  token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+};
 
 // create a pool
-const createPool = async() => {
+const createPool = async () => {
+  try {
     // Token Address Instance
     const tokenA = new Token(1, tokenAddresses.token0, 6, "USDC", "USD Coin");
-    const tokenA = new Token(1, tokenAddresses.token1, 18, "WETH", "Wrapped Ether");
+    const tokenB = new Token(
+      1,
+      tokenAddresses.token1,
+      18,
+      "WETH",
+      "Wrapped Ether"
+    );
 
     // Pool Fee
     const poolFee = await poolContract.fee();
@@ -40,8 +51,8 @@ const createPool = async() => {
     const nearestIndex = Math.floor(slot0[1] / tickSpacing) * tickSpacing;
 
     // Create a tick index
-    const tickLowerIndex = nearestIndex - (60 * 100);
-    const tickUpperIndex = nearestIndex + (60 * 100)
+    const tickLowerIndex = nearestIndex - 60 * 100;
+    const tickUpperIndex = nearestIndex + 60 * 100;
 
     // Tick Data
     const tickLowerData = await poolContract.ticks(tickLowerIndex);
@@ -49,31 +60,33 @@ const createPool = async() => {
 
     // Tick Instance
     const tickLower = new Tick({
-        index: tickLowerIndex,
-        liquidityGross: tickLowerData.liquidityGross,
-        liquidityNet: tickLowerData.liquidityNet
-    })
+      index: tickLowerIndex,
+      liquidityGross: tickLowerData.liquidityGross,
+      liquidityNet: tickLowerData.liquidityNet,
+    });
     const tickUpper = new Tick({
-        index: tickUpperIndex,
-        liquidityGross: tickUpperData.liquidityGross,
-        liquidityNet: tickUpperData.liquidityNet
-    })
+      index: tickUpperIndex,
+      liquidityGross: tickUpperData.liquidityGross,
+      liquidityNet: tickUpperData.liquidityNet,
+    });
 
     // Tick List
-    const tickList = new TickListDataProvider([tickLower, tickUpper], tickSpacing);
-    try {
-        const pool = new Pool(
-            tokenA,
-            tokenB,
-            poolFee,
-            slot0[0],
-            liquidity,
-            slot0[1],
-            tickList,
-        )
-        console.log(pool);
-    } catch (error) {
-        console.log(error);
-    }
-}
+    const tickList = new TickListDataProvider(
+      [tickLower, tickUpper],
+      tickSpacing
+    );
+    const pool = new Pool(
+      tokenA,
+      tokenB,
+      poolFee,
+      slot0[0],
+      liquidity,
+      slot0[1],
+      tickList
+    );
+    console.log(pool);
+  } catch (error) {
+    console.log(error);
+  }
+};
 createPool();
