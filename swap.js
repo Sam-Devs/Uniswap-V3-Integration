5f79ca12f9b8c3ee638f3bd62fc64a5debcd75f7
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-20 08:10:57 +0100

    swap.js file

--- swap.js
+++ swap.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { ethers } = require("ethers");
 const ISwapRouter = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json");
-const { CurrencyAmount } = require("@uniswap/sdk-core");
+const { CurrencyAmount, Percent } = require("@uniswap/sdk-core");
 const { Trade, Route } = require("@uniswap/v3-sdk");
 // Provider
 const mainnet = process.env.MAINNET;
@@ -45,19 +45,26 @@ const swap = async () => {
     console.log(`1 WETH can be swapped for ${route.midPrice.invert().toSignificant(6)} USDC`);
     const trade = new Trade.exactIn(route, amountIn);
     console.log(`The execution price of this trade is ${trade.executionPrice.toSignificant(6)} WETH for 1 USDC`);
+    
+    const slippageTolerance = new Percent("50", "10000");
+    
     // Amount Out Minimum
-    // const amountOutMinimum
-    // const swapParams = {
-    //   path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
-    //   recipient: signer.address,
-    //   deadline: deadline,
-    //   amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
-    //   amountOutMinimum:
-    // }
-    // const swapTransaction = router.exactInput(
-    //   swapParams,
-    //   {value: value, gasPrice: 20e9}
-    // );
+    const amountOutMinimum = trade.mininumAmountOut(slippageTolerance);
+    console.log(`For 5000 USDC you can get a minimum of ${amountOutMinimum.toSignificant(6)} WETH`);
+    const swapParams = {
+      path: Buffer.from([tokenAddresses.token0, tokenAddresses.token1]),
+      recipient: signer.address,
+      deadline: deadline,
+      amountIn: ethers.utils.parseUnits(amountIn.toExact(), 6),
+      amountOutMinimum: ethers.utils.parseUnits(amountOutMinimum.toExact(), 18)
+    }
+    const swapTransaction = router.exactInput(
+      swapParams,
+      {value: value, gasPrice: 20e9}
+    );
+    console.log(`Swap Transaction Hash: ${swapTransaction.hash}`);
+    const swapReceipt = await swapTransaction.wait()
+    console.log(`Swap Transaction Receipt: ${swapReceipt}`);
   } catch (error) {
     console.log(error);
   }
