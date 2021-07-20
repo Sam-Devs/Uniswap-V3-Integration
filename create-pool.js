eb45b61b7a875a6e33bb493bb6344cb69eff54df
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-20 05:39:45 +0100

    update

--- create-pool.js
+++ create-pool.js
@@ -9,18 +9,42 @@ const poolAddress = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8";
 const mainnet = process.env.MAINNET;
 const provider = new ethers.providers.JsonRpcProvider(mainnet);
 
+// Pool Contract
 const poolContract = new ethers.Contract(poolAddress, IUniswapV3Pool.abi, provider);
+
+// Token Address Object
+const tokenAddresses = {
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+}
 
 // create a pool
 const createPool = async() => {
+
+    // Token Addresses
+    const tokenA = new Token(1, tokenAddresses.token0, 6, "USDC", "USD Coin");
+    const tokenB = new Token(1, tokenAddresses.token1, 18, "WETH", "Wrapped Ether");
+
+    // Pool Fee
+    const poolFee = await poolContract.fee();
+
+    // Pool Price
+    const slot0 = await poolContract.slot0();
+
+    // Pool Liquidity
+    const poolLiquidity = await poolContract.liquidity();
+
+    // Pool Tick
+    // const poolTick = await poolContract.slot0();
+    
     // Pool Instance
     const pool = new Pool(
         tokenA:
         tokenB:
         poolFee:
-        poolPrice:
+        slot0[0]:
         poolLiquidity:
-        poolTick:
+        slot0[1]:
         tickList:
     )
 }
